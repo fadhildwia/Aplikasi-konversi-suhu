8ce255de8bd4c07d9578896e0b6576e176c9c374
Fadhilah Dwi Ananda <46224529+fadhildwia@users.noreply.github.com>
2020-09-07 18:44:58 +0700

    Add files via upload

---
+++ ConversiSuhu/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace conversi_suhu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Masukkan nilai ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(36, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(68, 19);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Celcius";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(36, 45);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(71, 19);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Reamur";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(36, 70);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(86, 19);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "Fahrenhiet";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(36, 95);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(60, 19);
+            this.checkBox4.TabIndex = 4;
+            this.checkBox4.Text = "Kelvin";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(96, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 40);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "CONVERT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonconvert);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(241, 247);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 40);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "RESET";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.buttonreset);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(291, 304);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(58, 24);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "EXIT";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.buttonexit);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(116, 21);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(129, 18);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(115, 21);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(129, 43);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(115, 21);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(128, 70);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(116, 21);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(128, 93);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(116, 21);
+            this.textBox5.TabIndex = 12;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Celcius",
+            "Reamur",
+            "Fahrenhiet",
+            "Kelvin"});
+            this.comboBox1.Location = new System.Drawing.Point(241, 67);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(106, 23);
+            this.comboBox1.TabIndex = 13;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(134, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 15);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "APLIKASI CONVERT SUHU ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox4);
+            this.groupBox1.Location = new System.Drawing.Point(96, 109);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(251, 132);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CEKLIS SUHU";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(380, 344);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.Name = "Form1";
+            this.Text = "Convert suhu";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ ConversiSuhu/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace conversi_suhu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonconvert(object sender, EventArgs e)
+        {
+            double celcius;
+            double reamur;
+            double fahrenheit;
+            double kelvin;
+
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+
+            if (comboBox1.SelectedItem == "Celcius")
+            {
+                celcius = Convert.ToInt32(textBox1.Text);
+                if (checkBox1.Checked == true)
+                    textBox2.Text = celcius.ToString();
+
+                if (checkBox2.Checked == true)
+                {
+                    reamur = 4 / 5.0 * celcius;
+                    textBox3.Text = reamur.ToString();
+                }
+                if (checkBox3.Checked == true)
+                {
+                    fahrenheit = 9 / 5.0 * celcius + 32;
+                    textBox4.Text = fahrenheit.ToString();
+                }
+                if (checkBox4.Checked == true)
+                {
+                    kelvin = celcius + 273;
+                    textBox5.Text = kelvin.ToString();
+                }
+            }
+
+            if (comboBox1.SelectedItem == "Reamur")
+            {
+                reamur = Convert.ToInt32(textBox1.Text);
+                if (checkBox1.Checked == true)
+                {
+                    celcius = 5 / 4.0 * reamur;
+                    textBox2.Text = celcius.ToString();
+                }
+                if (checkBox2.Checked == true)
+                    textBox3.Text = reamur.ToString();
+
+                if (checkBox3.Checked == true)
+                {
+                    fahrenheit = 9 / 4.0 * reamur + 32;
+                    textBox4.Text = fahrenheit.ToString();
+                }
+                if (checkBox4.Checked == true)
+                {
+                    kelvin = 5 / 4.0 * reamur + 273;
+                    textBox5.Text = kelvin.ToString();
+                }
+            }
+            if (comboBox1.SelectedItem == "Fahrenheit")
+            {
+                fahrenheit = Convert.ToInt32(textBox1.Text);
+                if (checkBox1.Checked == true)
+                {
+                    celcius = 5 / 9.0 * (fahrenheit - 32);
+                    textBox2.Text = celcius.ToString();
+                }
+                if (checkBox2.Checked == true)
+                {
+                    reamur = 4 / 9.0 * (fahrenheit - 32);
+                    textBox3.Text = reamur.ToString();
+                }
+
+                if (checkBox3.Checked == true)
+                {
+                    textBox4.Text = fahrenheit.ToString();
+                }
+                if (checkBox4.Checked == true)
+                {
+                    kelvin = 5 / 9.0 * (fahrenheit - 32) + 273;
+                    textBox5.Text = kelvin.ToString();
+                }
+            }
+
+            if (comboBox1.SelectedItem == "Kelvin")
+            {
+                kelvin = Convert.ToInt32(textBox1.Text);
+                if (checkBox1.Checked == true)
+                {
+                    celcius = kelvin - 273;
+                    textBox2.Text = celcius.ToString();
+                }
+                if (checkBox2.Checked == true)
+                {
+                    reamur = 4 / 5.0 * (kelvin - 273);
+                    textBox3.Text = reamur.ToString();
+                }
+                if (checkBox3.Checked == true)
+                {
+                    fahrenheit = 9 / 5.0 * (kelvin - 273) + 32;
+                    textBox4.Text = fahrenheit.ToString();
+                }
+                if (checkBox4.Checked == true)
+                    textBox5.Text = kelvin.ToString();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonreset(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            comboBox1.Text = "";
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+        }
+
+        private void buttonexit(object sender, EventArgs e)
+        {
+            DialogResult keluar = MessageBox.Show("Yakin ingin keluar?", "Peringatan", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+            if (keluar == DialogResult.Yes)
+            {
+                Close();
+            }
+            else
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+                textBox5.Text = "";
+                comboBox1.Text = "";
+                checkBox1.Checked = false;
+                checkBox2.Checked = false;
+                checkBox3.Checked = false;
+                checkBox4.Checked = false;
+            }
+        }
+
+    }
+}
+    
